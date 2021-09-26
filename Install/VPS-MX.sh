082a2d0583bf8d2cc80dfebb0a36938ca469f2b6
ADM-PERU <90083965+ADM-PERU@users.noreply.github.com>
2021-09-26 14:43:10 -0500

    Update VPS-MX.sh

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -244,12 +244,12 @@ echo 'echo "" '>> .bashrc
 echo 'mess1="$(less /etc/newadm/message.txt)" ' >> .bashrc
 echo 'echo "" '>> .bashrc
 echo 'echo -e "\033[0;33m 🔺Script optimizado para el buen uso con una correcta configuración. "'>> .bashrc
-echo 'echo -e "\033[0;33m 🔺Acceso PRÉMIUM tiene soporte con el dueño oficial ✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹. "'>> .bashrc
+echo 'echo -e "\033[0;33m 🔺Acceso PRÉMIUM tiene soporte con el dueño oficial ✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹.:::::: "'>> .bashrc
 echo 'echo -e "\033[0;33m 🔺Key free no tiene soporte alguno. "'>> .bashrc
 echo 'echo -e "\033[1;35m    "'>> .bashrc
-echo 'echo -e "\033[0;34m   ✅𝐑𝐄𝐒𝐄𝐋𝐋𝐄𝐑 : $mess1 "'>> .bashrc
+echo 'echo -e "\033[0;30m  ✅RESELLER : $mess1 "'>> .bashrc
 echo 'echo "" '>> .bashrc                                               
-echo 'echo -e "\033[1.37m  PARA MOSTAR EL PANEL DE CONTROL ESCRIBA:  menu "'>> .bashrc
+echo 'echo -e "\033[0;30m   PARA MOSTRAR EL PANEL DE CONTROL ESCRIBA:  menu "'>> .bashrc
 echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com/ADM-PERU/VIP/main/VerScrpt/VercUp &>/dev/null'>> .bashrc
 echo 'echo ""'>> .bashrc
 echo -e "         COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
