896443e5034368031f78c6d87b0da2728084285f
ADM-PERU <90083965+ADM-PERU@users.noreply.github.com>
2021-09-26 14:59:35 -0500

    Update VPS-MX.sh

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -249,7 +249,7 @@ echo 'echo -e "\033[0;33m 🔺Key free no tiene soporte alguno. "'>> .bashrc
 echo 'echo -e "\033[1;35m    "'>> .bashrc
 echo 'echo -e "\033[0;36m  ✅RESELLER : $mess1 "'>> .bashrc
 echo 'echo "" '>> .bashrc                                               
-echo 'echo -e "\033[0;36m   PARA MOSTRAR EL PANEL DE CONTROL ESCRIBA:  menu "'>> .bashrc
+echo 'echo -e "\033[0;36m  ✅PARA MOSTRAR EL PANEL DE CONTROL ESCRIBA:  menu "'>> .bashrc
 echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com/ADM-PERU/VIP/main/VerScrpt/VercUp &>/dev/null'>> .bashrc
 echo 'echo ""'>> .bashrc
 echo -e "         COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
