cfa43c61fe4538f71607a83c7267d9c157b5b232
ADM-PERU <90083965+ADM-PERU@users.noreply.github.com>
2022-01-18 14:13:27 -0600

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -141,22 +141,21 @@ number=$(expr length $1)
 for((i=1; i<$number+1; i++)); do
 txt[$i]=$(echo "$1" | cut -b $i)
 case ${txt[$i]} in
-".")txt[$i]="+";;
-"+")txt[$i]=".";;
+".")txt[$i]="x";;
+"x")txt[$i]=".";;
 "1")txt[$i]="@";;
 "@")txt[$i]="1";;
 "2")txt[$i]="?";;
 "?")txt[$i]="2";;
-"4")txt[$i]="%";;
-"%")txt[$i]="4";;
-"-")txt[$i]="K";;
-"K")txt[$i]="-";;
+"4")txt[$i]="0";;
+"0")txt[$i]="4";;
+"/")txt[$i]="K";;
+"K")txt[$i]="/";;
 esac
 txtofus+="${txt[$i]}"
 done
 echo "$txtofus" | rev
 }
-
 
 gerar_key () {
 valuekey="$(date | md5sum | head -c10)"
