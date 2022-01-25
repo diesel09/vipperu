82feb08888d58baa7c716c770d85dfe01ef94360
ADM-PERU <90083965+ADM-PERU@users.noreply.github.com>
2022-01-25 11:23:53 -0500

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -6,6 +6,44 @@ SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FETS1QRVJVL1ZJUC9tYWluL2d
 SUB_DOM='base64 -d'
 rm $(pwd)/$0
 
+check_ip () {
+MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MIP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+echo "$IP" > /usr/bin/vendor_code
+}
+
+function_verify () {
+#echo -e "verificando..."
+# check_ip
+ permited=$(curl -sSL "https://raw.githubusercontent.com/ADM-PERU/VIP/main/Control/Control-IP")
+  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+  clear
+  echo -e "\n\n\n\e[31m====================================================="
+  echo -e "\e[31m         ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADO!\n     SI DESEAS USAR EL GENERADOR CONTACTE A @GENKEY_BOT\n                       VIA TELEGRAM."
+  echo -e "\e[31m=====================================================\n\n\n\e[0m"
+  [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
+[[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
+  exit 1
+  } || {
+ ### INTALAR VERCION DE SCRIPT
+  clear
+  echo -e "\n\n\n\e[32m====================================================="
+  echo -e "\e[32m        ¡LA IP $(wget -qO- ipv4.icanhazip.com) ESTA AUTORIZADA!\n   AUTORIZADO CORRECTAMENTE PARA EL USO DEL GENERADOR.\n                   ESPERE PORFAVOR..."
+  echo -e "\e[32m=====================================================\n\n\n\e[0m"
+  sleep 5
+  CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
+  v1=$(curl -sSL "https://raw.githubusercontent.com/ADM-PERU/MGRBOT/main/Vercion")
+  echo "$v1" > /etc/ADM-db/vercion
+  echo "@GENKEY_BOT" > ${CIDdir}/resell
+  
+  }
+}
+
+echo -e "verificando..."
+check_ip
+function_verify
+
 ofus () {
 unset server
 server=$(echo ${txt_ofuscatw}|cut -d':' -f1)
